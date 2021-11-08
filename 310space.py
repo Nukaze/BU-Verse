c869ed6478a849089832ed73e0c7cff625966f66
Kaedehara Nukaze <acezgtx7@gmail.com>
2021-11-08 14:27:52 +0700

    signup optimize and init login

--- 310space.py
+++ 310space.py
@@ -5,13 +5,15 @@ import os
 import time
 
 feedlst, postlst, disnamelst = [], [], []                                   # contentfeed
-userlst, keylst, uidlst, sessionlst  = [], [], [], []                       # LoginSystem
+userlst, pwkeylst, idclst, sessionlst  = [], [], [], []                       # LoginSystem
 namelst, snamelst, majorlst, facultylst, majorlst = [], [], [], [], []      # Personal data
 adminlst = []
 uname = ""
 timer,idx = 0,0
 uiclear = lambda: os.system('cls')
 headsignup = ("+" * 64 + "\n" + "< [ BU-Verse Sign-up ] >".center(64) + "\n" + "+" * 64)
+headlogin = ("|"*64 + "\n" + "< [ BU-Verse Login ] >".center(64) + "\n" + "|"*64)
+
 
 def loading_progress(timer,interval,delay):
     print("[",end="")
@@ -42,14 +44,15 @@ def buverse_main():
             print("[1] Login")
             print("[2] Sign-up")
             print("[0] Exit the Program")
+            print(userlst, pwkeylst, idclst, disnamelst)
             gout = input("Press select menu : ")
             if gout == "1":
                 print("[ Going to Login.. ]")
-                loading_progress(4,0.2,1)
+                loading_progress(4,0.1,0.2)
                 buverse_login()
             elif gout == "2":
                 print("[ Going to Sign-up.. ]")
-                loading_progress(4,0.4,1)
+                loading_progress(4,0.3,0.5)
                 buverse_signup()
             elif gout == "0":
                 time.sleep(0.25)
@@ -58,7 +61,47 @@ def buverse_main():
                 print("."*64)
                 exit("[ Exiting the BU-Verse.. ]".center(64) + "\n"+"."*64)
             else:
-                input("!! Invalid Menu, Please any key to try again.")
+                print("!! Invalid Menu, Please try again.")
+                time.sleep(1)
+
+def buverse_signup():       # 0000000000000
+    uiclear()
+    print(headsignup)
+    uname = input("Enter Your Username \n> ")
+    with open('buvs_userdb.txt','r') as dbuser:
+        if uname not in dbuser:
+            userlst.append(uname)
+            idx = userlst.index(uname)
+            print(userlst)
+            buverse_getpwkey(idx)
+        else:
+            strx64 = "x"*64
+            print(strx64.center(64))
+            print("[ Sorry, this Username is already taken. ]".center(64))
+            print("[ Please try again. ]".center(64))
+            print(strx64.center(64))
+            print("")
+            buverse_signup()
+        #buverse_getpwkey()
+        idc = input("Enter Your ID card Number (Must be number and more equal 13 character) \n> ")
+        while len(idc) != 13 or idc.isalpha():
+            print("ID card must have 13 characters and number only")
+            idc = input("Enter Your ID card Number (Must be number and more equal 13 character) \n> ")
+        idclst.append(idc)
+        disname = input("Enter Your Name to display on profile (Can edit after) \n> ")
+        disnamelst.append(disname)
+        print(userlst, pwkeylst, idclst, disnamelst)
+        dbuser = open('buvs_userdb.txt', 'w')
+        dbkey = open('buvs_keydb.txt', 'w')
+        dbidc = open('buvs_idcdb.txt', 'w')
+        dbuser.write(userlst[idx]+""+disnamelst[idx])
+        dbkey.write(pwkeylst[idx])
+        dbidc.write(idclst[idx])
+        dbuser.close()
+        dbkey.close()
+        dbidc.close()
+    print("[ BU-Verse Sign-up Successfully. ]")
+    print("\n"*5)
 
 def buverse_getpwkey(idx):
     pwkey1 = getpass.getpass("Enter Your Password (Must have more than 8 characters) \n> ")
@@ -70,50 +113,23 @@ def buverse_getpwkey(idx):
         pwkey1 = getpass.getpass("Enter Your Password (Must have more than 8 characters) \n> ")
     pwkey2 = getpass.getpass("Confirm Your Password (Must have more than 8 characters) \n> ")
     if pwkey1 == pwkey2:
-        keylst.append(pwkey1)
+        pwkeylst.append(pwkey1)
     else:
-        os.system('cls')
+        uiclear()
+        print(headsignup)
         print("Enter Your Username \n> " + userlst[idx])
         print("[ # Those passwords didn't match!! Please try again. ]")
         buverse_getpwkey(idx)
 
-def buverse_signup():       # 0000000000000
-    uiclear()
-    print(headsignup)
-    uname = input("Enter Your Username \n> ")
-    dbuser = open('buvs_userdb.txt','r+')
-    if uname not in dbuser:
-        userlst.append(uname)
-        global idx
-        idx = userlst.index(uname)
-        print(userlst)
-        buverse_getpwkey(idx)
-    else:
-        strx64 = "x"*64
-        print(strx64.center(64))
-        print("[ Sorry, this Username is already taken. ]".center(64))
-        print("[ Please try again. ]".center(64))
-        print(strx64.center(64))
-        print("")
-        buverse_signup()
-    #buverse_getpwkey()
-    uid = input("Enter Your ID card Number (Must be number and more equal 13 character) \n> ")
-    while len(uid) != 13 or uid.isalpha():
-        print("ID card must have 13 characters and number only")
-        uid = input("Enter Your ID card Number (Must be number and more equal 13 character) \n> ")
-    uid
-    disname = input("Enter Your Name to display on profile (Can edit after) \n> ")
-
-    dbuser.close()
-    print("[ BU-Verse Sign-up Successfully. ]")
-    print("\n"*5)
-
 def buverse_login():
     uiclear()
-    print("|"*64)
-    print("< [ BU-Verse Login ] >".center(64))
-    print("|"*64)
-    print("\n" * 5)
+    print(headlogin)
+    uname = input("USERNAME : ".rjust(24))
+    pwkey = input("PASSWORD : ".rjust(24))
+    if uname in userlst:
+        pass
+    print("\n" * 3)
+    input("go menu")
 
 
 """
